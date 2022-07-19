77a6c1abe5e461ea459c421fc179d4bedae2f25a
thanhngo9 <ngo.thanh80@yahoo.com>
2022-07-19 17:55:30 -0400

    commit

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 const Discord = require("discord.js");
 const axios = require("axios");
 const token = require("./token.js");
-const { s } = require("@sapphire/shapeshift");
-const client = new Discord.Client({intents:["GUILDS", "GUILD_MESSAGES"]});
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 const prefix = "^";
 
 client.login(token);
-client.on("ready", async() => {  
-    console.log("test");
- });
+client.on("ready", async () => {
+  console.log("test");
+});
 
- client.on("messageCreate", (message) => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return; 
-   
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-   
-    if(command == "cats"){
-        message.channel.send("hi");
-    } else if(command != "cats"){
-       sendMessage(message.author.username, message.channel.id);
+client.on("messageCreate", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  switch (command) {
+    case "help": {
+      client.channels.cache.get(message.channel.id).send("help");
+      break;
     }
-})
+    case "info": {
+      client.channels.cache.get(message.channel.id).send("info");
+      break;
+    }
+    default: {client.channels.cache.get(message.channel.id).send(message.author.username + ", that is not a real command");
+    break;}
+  }
 
-function sendMessage(username, channelId) {
-    client.channels.cache.get(channelId).send(username + ", that is not a command!");
-}
-console.log("hi");
+});
